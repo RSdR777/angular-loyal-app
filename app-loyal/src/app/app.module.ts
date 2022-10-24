5666cc2c59d98730d4e7d1e7e541321b4fb7f6eb
RSdR777 <rsrosal@minsail.com>
2022-10-24 09:20:29 +0200

    ng new aplicación solucionamos error

--- app-loyal/src/app/app.module.ts
+++ app-loyal/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './component/users/users.component';
 import { LoginComponent } from './component/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
+  /* definir las vistas que forman parte del módulo*/
   declarations: [
     AppComponent,
-    UsersComponent,
-    LoginComponent
+    UsersComponent,  /* ng g c users */
+    LoginComponent   /* ng g c login */
   ],
+  /* export de otros módulos que importamos */
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  /*servicios qie se pueden acceder desde cualquier parte de la aplicación (por ser el módulo raíz)*/
   providers: [],
+  /*componoente inicial de la aplicación (el 'play'). Componente que arranca */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
